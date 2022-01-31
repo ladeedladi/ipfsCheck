257aa7c7a9344a545535a0bbe90eed483138a5b8
Ladeed <muhammed.ladeed123@gmail.com>
2022-01-31 15:07:55 +0530

    added ipfs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ipfs from "./ipfs"
+import React,{useState} from 'react';
+var Buffer = require('buffer/').Buffer
 function App() {
+  const [file,setFile]=useState(null)
+  const [ipfsHash,setIpfshash]=useState("")
+  const changeFile=(e)=>{
+  setFile(e.target.files[0])
+
+  const ifile = e.target.files[0]
+  const reader = new window.FileReader()
+  reader.readAsArrayBuffer(ifile)
+  reader.onloadend = () => {
+    setFile(Buffer(reader.result))
+    console.log('buffer')
+  }
+  }
+
+  const fileSubmit=async()=>{
+   console.log(file);
+    const result=await ipfs.add(file)
+    console.log(result);
+    console.log("ipfsHash",result.path);
+    setIpfshash(result.path)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+       <img src={`https://ipfs.io/ipfs/${ipfsHash}`} alt="" />
+      <input type="file" onChange={changeFile}/>
+      <button onClick={fileSubmit}>click</button>
     </div>
   );
 }
---
+++ src/ipfs.js
@@ -0,0 +1,7 @@
+// const IPFS=require("ipfs-http-client")
+import { create } from 'ipfs-http-client'
+const ipfs = create({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
+
+
+
+export default ipfs
